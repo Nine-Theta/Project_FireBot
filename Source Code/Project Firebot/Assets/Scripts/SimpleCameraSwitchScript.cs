bb17c5ba4f0f680c717261d7b4926db222574482
David <davidheetebrij@gmail.com>
2020-05-06 04:26:58 +0200

    Start of a UI scene, basics are more or less running. Added some folders.

---
+++ Source Code/Project Firebot/Assets/Scripts/SimpleCameraSwitchScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//prototype script, not intended for release
+
+public class SimpleCameraSwitchScript : MonoBehaviour
+{
+    [SerializeField] private Camera MainCam;
+    [SerializeField] private CamSwitch[] Cameras;
+    private void Start()
+    {
+        MainCam = Cameras[0].cam;
+        Cameras[0].cam.enabled = true;
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < Cameras.Length; i++)
+        {
+            if (Input.GetKeyDown(Cameras[i].key))
+            {
+                MainCam = Cameras[i].cam;
+                Cameras[i].cam.enabled = true;
+            }
+
+            if (MainCam != Cameras[i].cam) Cameras[i].cam.enabled = false;
+        }
+    }
+}
+
+[System.Serializable]
+public class CamSwitch
+{
+    [SerializeField] public Camera cam;
+    [SerializeField] public KeyCode key;
+}
+
+
+[CustomPropertyDrawer(typeof(CamSwitch))]
+public class DamageValuesDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // Using BeginProperty / EndProperty on the parent property means that
+        // __prefab__ override logic works on the entire property.
+        EditorGUI.BeginProperty(position, label, property);
+
+        // Draw label
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        // Calculate rects
+        Rect typeRect = new Rect(position.x, position.y, 100, position.height);
+        Rect valueRect = new Rect(position.x + 100, position.y, 90, position.height);
+
+        // Draw fields - passs GUIContent.none to each so they are drawn without labels
+        EditorGUI.PropertyField(typeRect, property.FindPropertyRelative("cam"), GUIContent.none);
+        EditorGUI.PropertyField(valueRect, property.FindPropertyRelative("key"), GUIContent.none);
+
+        EditorGUI.EndProperty();
+    }
+}
---
+++ Source Code/Project Firebot/Assets/Scripts/SimplePlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//prototype script, not intended for release
+
+//[RequireComponent(typeof(Rigidbody))]
+public class SimplePlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform CamTransform;
+    [SerializeField] private Rigidbody body;
+
+    void Start()
+    {
+        //body = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,Input.GetAxis("Mouse X"),0);
+        CamTransform.Rotate(-Input.GetAxis("Mouse Y"), 0, 0);
+        body.AddRelativeForce(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        
+    }
+}
